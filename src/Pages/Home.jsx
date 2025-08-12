3acc988fe833441ab108d16ca6dedb374ef5fefe
Aparna Sharma <66220639+codewithaparna@users.noreply.github.com>
2025-08-12 22:38:50 +0530

    Add files via upload

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,28 @@
+function Home() {
+  return (
+  <>
+
+            <img src="images/yoga.jpg" alt="yoga"/>
+
+                <h2>YOGA</h2>
+
+                <p>Yoga is essentially a spiritual discipline based on an extremely subtle Science which focuses on bringing harmony between 
+                    mind and body. It is an art and science for healthy living. The word "Yoga" is derived from the Sanskrit root yuj meaning 
+                    "to join", "to yoke" or "to unite".
+
+                    According to Yogic scriptures, the practice of Yoga leads to the union of individual consciousness with universal 
+                    consciousness. According to modern scientists, everything in the universe is just a manifestation of the same quantum 
+                    firmament. One who experiences this oneness of existence is said to be "in Yoga" and is termed as a yogi who has attained
+                    a state of freedom, referred to as Mukti, nirvāna, kaivalya or moksha.
+                    
+                    "Yoga" also refers to an inner science comprising of a variety of methods through which human beings can achieve union 
+                    between the body and mind to attain self-realisation. The aim of Yoga practice (sādhana) is to overcome all kinds of 
+                    sufferings that lead to a sense of freedom in every walk of life with holistic health, happiness and harmony.
+                    
+                    </p>
+                    
+        </>
+  )
+}
+
+export default Home;
---
+++ src/Pages/PageTwo.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+function PageTwo() {
+  return (
+    <>
+
+            <img src="images/drink1.jpg" alt="drink 1"/>
+
+                <h2>SMOOTHIES</h2>
+
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus consectetur dolorem optio expedita natus, doloribus 
+                    veniam, reiciendis unde est distinctio et facere quod eaque. Est tempore rerum nostrum doloribus? Vero expedita cum deserunt 
+                    dolorem possimus maxime soluta cupiditate repellat libero dicta! Distinctio illo, non architecto totam eos ratione qui eius
+                     praesentium explicabo numquam tempore perferendis molestias culpa? Veritatis architecto consequuntur quisquam. At, ad neque 
+                     ut quia unde delectus voluptates vero distinctio, sed nihil culpa quos error quo nemo voluptatem qui nam mollitia alias 
+                     repellendus cumque! Similique laboriosam ratione veritatis? Aliquid necessitatibus accusantium eligendi dolor. Minus sed 
+                     laborum tenetur repudiandae pariatur culpa necessitatibus ipsam ex repellat obcaecati veritatis in perspiciatis doloremque 
+                     eaque odio deserunt, vel exercitationem nobis rem officiis. Perferendis sunt facere repellendus nemo iusto magni eum aut, 
+                     voluptates maiores recusandae. Officiis ducimus eius illo voluptatibus corrupti mollitia debitis excepturi ea asperiores 
+                     voluptates? Dolorem, assumenda quasi blanditiis perspiciatis doloribus sint reiciendis laudantium magnam quisquam minima 
+                     eum rerum cumque ullam aliquid dolore fugit ex voluptates eligendi autem in inventore itaque tempora ut. Excepturi ducimus 
+                     amet quod corrupti, quidem consequatur omnis pariatur alias! Dolorum deserunt et optio repudiandae ipsa expedita ipsum iusto 
+                     ducimus voluptatibus soluta esse qui perspiciatis accusantium aperiam consequatur ea fuga, sapiente, deleniti at pariatur ab 
+                     nobis velit porro. Qui quae tempora repellat quia consequuntur, eum quidem eligendi sequi deserunt aliquam nulla dicta 
+                     corrupti dolorum culpa sunt dolores inventore nemo iusto. Quo magni, repellat nisi minima, aliquid ratione temporibus eum, 
+                     blanditiis laudantium pariatur maxime numquam doloremque dolorum nemo odit delectus quam?
+                    
+                    </p>
+                    <hr/>
+
+                    <img src="images/drink2.jpg" alt="drink 2"/>
+
+                <h2>HEALTHY DRINKS</h2>
+
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus consectetur dolorem optio expedita natus, doloribus 
+                    veniam, reiciendis unde est distinctio et facere quod eaque. Est tempore rerum nostrum doloribus? Vero expedita cum deserunt 
+                    dolorem possimus maxime soluta cupiditate repellat libero dicta! Distinctio illo, non architecto totam eos ratione qui eius
+                     praesentium explicabo numquam tempore perferendis molestias culpa? Veritatis architecto consequuntur quisquam. At, ad neque 
+                     ut quia unde delectus voluptates vero distinctio, sed nihil culpa quos error quo nemo voluptatem qui nam mollitia alias 
+                     repellendus cumque! Similique laboriosam ratione veritatis? Aliquid necessitatibus accusantium eligendi dolor. Minus sed 
+                     laborum tenetur repudiandae pariatur culpa necessitatibus ipsam ex repellat obcaecati veritatis in perspiciatis doloremque 
+                     eaque odio deserunt, vel exercitationem nobis rem officiis. Perferendis sunt facere repellendus nemo iusto magni eum aut, 
+                     voluptates maiores recusandae. Officiis ducimus eius illo voluptatibus corrupti mollitia debitis excepturi ea asperiores 
+                     voluptates? Dolorem, assumenda quasi blanditiis perspiciatis doloribus sint reiciendis laudantium magnam quisquam minima 
+                     eum rerum cumque ullam aliquid dolore fugit ex voluptates eligendi autem in inventore itaque tempora ut. Excepturi ducimus 
+                     amet quod corrupti, quidem consequatur omnis pariatur alias! Dolorum deserunt et optio repudiandae ipsa expedita ipsum iusto 
+                     ducimus voluptatibus soluta esse qui perspiciatis accusantium aperiam consequatur ea fuga, sapiente, deleniti at pariatur ab 
+                     nobis velit porro. Qui quae tempora repellat quia consequuntur, eum quidem eligendi sequi deserunt aliquam nulla dicta 
+                     corrupti dolorum culpa sunt dolores inventore nemo iusto. Quo magni, repellat nisi minima, aliquid ratione temporibus eum, 
+                     blanditiis laudantium pariatur maxime numquam doloremque dolorum nemo odit delectus quam?
+                    
+                    </p>
+        </>
+
+  )
+}
+
+export default PageTwo
